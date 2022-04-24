4a8ecfabe20f20cc36a5b51b20a55b46e2c1af98
MatheusOrange211 <matheusnaranjocorrea@gmail.com>
2022-04-24 16:05:21 -0400

    first commit of project

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-const http = require('http');
-const port = 3000;
+import app from "./src/app.js";
+
+const port = process.env.port || 3000;
 
 
-const routes = {
-    "/":"bem vindo",
-    "/livros": "pagina de livros",
-    "/sobre": "pagina sobre nos"
-}
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/plain' });
-    res.end(routes[req.url]);
-})
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}.`);
 })
